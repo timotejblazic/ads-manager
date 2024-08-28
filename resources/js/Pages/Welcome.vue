f959ccb5a510fb8331b653a9692bc4b102dfdbc7
Timotej <timi.blazic@gmail.com>
2024-08-28 16:01:25 +0200

    added laravel breeze with inertiajs with vuejs components

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+});
+</script>
+
+<template>
+    <Head title="Welcome" />
+
+        <div v-if="canLogin" class="sm:fixed sm:top-0 sm:right-0 p-6 text-end">
+            <Link
+                v-if="$page.props.auth.user"
+                :href="route('dashboard')"
+                class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                >Dashboard</Link
+            >
+
+            <template v-else>
+                <Link
+                    :href="route('login')"
+                    class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                    >Log in</Link
+                >
+
+                <Link
+                    v-if="canRegister"
+                    :href="route('register')"
+                    class="ms-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
+                    >Register</Link
+                >
+            </template>
+        </div>
+
+        <div class="max-w-7xl mx-auto p-6 lg:p-8">
+            <h1>This is my app</h1>
+        </div>
+</template>
+
+<style>
+
+</style>
